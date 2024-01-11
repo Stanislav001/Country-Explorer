a75724807ab7198570a6a743d41093e2557a2619
Stanislav001 <stanislav.stanchev@netea.bg>
2024-01-11 16:49:22 +0200

    Filter screen design tweaks

--- src/screens/filter/Filter.jsx
+++ src/screens/filter/Filter.jsx
@@ -1,19 +1,26 @@
 import { useState } from 'react';
 import styles from './filter.styles';
-import { Feather } from '@expo/vector-icons';
-import { COLORS, TEXT, SIZES } from '../../constants/theme';
-import { HeightSpacer } from '../../components';
 import AppBar from '../../components/Reusable/AppBar';
 import { useSearchHotels } from '../../hooks/useHotel';
 import { useSearchPlaces } from '../../hooks/usePlace';
-import ReusableTitle from '../../components/Reusable/ReusableTitle';
-import ReusableText from '../../components/Reusable/ReusableText';
-import { DescriptionText } from '../../components';
-import { View, TextInput, FlatList, TouchableOpacity, Image, SafeAreaView, } from 'react-native';
 import reusable from '../../components/Reusable/reusable';
+import { COLORS, TEXT, SIZES } from '../../constants/theme';
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import { DescriptionText, ReusableBtn, ReusableText } from '../../components';
+import { AntDesign, FontAwesome, MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
+
+const rectanglesData = [1, 2, 3, 4, 5];
+const amenitiesData = [
+    { text: '24h Front-Desk', icon: <AntDesign name="infocirlceo" size={18} color="black" /> },
+    { text: 'Free Parking', icon: <MaterialCommunityIcons name="parking" size={18} color="black" /> },
+    { text: 'Free Wi-Fi Zone', icon: <AntDesign name="wifi" size={18} color="black" /> },
+    { text: 'Pub Restaurant', icon: <Ionicons name="md-restaurant-sharp" size={18} color="black" /> },
+];
 
 const Filter = ({ navigation }) => {
     const [searchKey, setSearchKey] = useState('');
+    const [selectedAmenities, setSelectedAmenities] = useState('');
+    const [selectedStarOption, setSelectedStarOption] = useState('');
     const { data: searchResult, isLoading, isError, error } = useSearchPlaces(searchKey);
 
     return (
@@ -39,6 +46,56 @@ const Filter = ({ navigation }) => {
                         color={COLORS.gray}
                         text={'Price Range'} />
 
+                    <View style={{ borderBottomColor: COLORS.lightGrey, marginVertical: 15, borderBottomWidth: 1 }} />
+
+                    <ReusableText
+                        family={'regular'}
+                        size={SIZES.large}
+                        color={COLORS.gray}
+                        text={'Star Rating'} />
+
+                    <View style={reusable.rowWithSpace('center')}>
+                        {rectanglesData.map((data, index) => (
+                            <TouchableOpacity key={index} style={styles.rectangle(data === selectedStarOption)} onPress={() => setSelectedStarOption(data)}>
+                                <ReusableText
+                                    text={data}
+                                    family={'regular'}
+                                    size={SIZES.medium}
+                                    color={COLORS.black} />
+                                <FontAwesome name="star" size={18} color="yellow" />
+                            </TouchableOpacity>))}
+                    </View>
+
+                    <View style={{ borderBottomColor: COLORS.lightGrey, marginVertical: 15, borderBottomWidth: 1 }} />
+
+                    <ReusableText
+                        family={'regular'}
+                        size={SIZES.large}
+                        color={COLORS.gray}
+                        text={'Amenities & Facilities'} />
+
+                    <View style={reusable.rowWithSpace('center')}>
+                        {amenitiesData?.map((data, index) => (
+                            <TouchableOpacity key={index} style={{ alignItems: 'center' }} onPress={() => setSelectedAmenities(data?.text)}>
+                                <View style={styles.circle(selectedAmenities === data?.text)}>
+                                    {data?.icon}
+                                </View>
+                                <Text style={styles.text}>{data?.text}</Text>
+                            </TouchableOpacity>
+                        ))}
+                    </View>
+
+                    <View style={{ borderBottomColor: COLORS.lightGrey, marginVertical: 15, borderBottomWidth: 1 }} />
+
+                    <ReusableBtn
+                        borderWidth={0}
+                        textColor={COLORS.white}
+                        width={SIZES.width - 40}
+                        borderColor={COLORS.green}
+                        btnText={"Apply"}
+                        backgroundColor={COLORS.green}
+                        onPress={() => { }}
+                    />
                 </View>
             </View>
         </SafeAreaView>
--- src/screens/filter/filter.styles.js
+++ src/screens/filter/filter.styles.js
@@ -10,6 +10,54 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     marginHorizontal: 12,
   },
+  rectangle: (isSelected) => ({
+    width: 50,
+    height: 35,
+    backgroundColor: isSelected ? COLORS.green : COLORS.lightGrey,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    margin: 10,
+    borderRadius: 5,
+  }),
+  number: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  icon: {
+    marginTop: 5,
+  },
+  circle: (isSelected) => ({
+    width: 50,
+    height: 50,
+    borderRadius: 30,
+    backgroundColor: isSelected ? COLORS.green : COLORS.lightGrey,
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 10,
+  }),
+  // circle: (isSelected) => ({
+  //   width: 50,
+  //   height: 50,
+  //   borderRadius: 30,
+  //   backgroundColor: COLORS.green,
+  //   justifyContent: "center",
+  //   alignItems: "center",
+  //   margin: 10,
+  //   borderRadius: 5,
+  // }),
+  number: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  text: {
+    marginTop: 5,
+    maxWidth: 80,
+    marginHorizontal: 10,
+    textAlign: "center",
+    flexWrap: "wrap",
+  },
 });
 
 export default styles;
